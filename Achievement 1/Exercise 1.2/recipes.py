d32714eff5982b5c28d7e41c60850f84b56a80ac
LeandraMT <magantierleandra@gmail.com>
2023-11-16 23:22:08 +0100

    added new folder 1.2 and corresponding tasks

---
+++ Achievement 1/Exercise 1.2/recipes.py
@@ -0,0 +1,58 @@
+all_recipes = []
+
+recipe_1 = {
+    "Name": "Tea",
+    "Cooking time": 5,
+    "Ingredients": ["Tea leaves", "Sugar", "Water"],
+}
+
+all_recipes.append(recipe_1)
+
+recipe_2 = {
+    "Name": "Banana Berry Smoothie",
+    "Cooking time": 2,
+    "Ingredients": ["Frozen bananas", "Mixed berries", "Yoghurt", "Milk", "Honey"],
+}
+
+all_recipes.append(recipe_2)
+
+recipe_3 = {
+    "Name": "Mango and Coconut Smoothie",
+    "Cooking time": 2,
+    "Ingredients": [
+        "Frozen mango chunks",
+        "Coconut milk",
+        "Yoghurt",
+        "Honey",
+        "Lime juice",
+    ],
+}
+
+all_recipes.append(recipe_3)
+
+recipe_4 = {
+    "Name": "Apple and Cinnamon Müsli Bowl",
+    "Cooking time": 3,
+    "Ingredients": [
+        "Oats",
+        "Milk",
+        "Water",
+        "Apple",
+        "Cinnamon",
+        "Brown sugar",
+        "Nuts",
+    ],
+}
+
+all_recipes.append(recipe_4)
+
+recipe_5 = {
+    "Name": "Berry and Yoghurt Müsli Bowl",
+    "Cooking time": 3,
+    "Ingredients": ["Müsli", "Yoghurt", "Mixed berries", "Honey", "Mint"],
+}
+
+all_recipes.append(recipe_5)
+
+
+print(all_recipes)
